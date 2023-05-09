5dde6a57e0c49b88432f8933d75a4cc6d19f04b5
manoj <manojsahoo890880@gmail.com>
2023-05-09 15:13:11 +0530

    done

--- js/script.js
+++ js/script.js
@@ -26,4 +26,4 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:350, origin:'left'})
 sr.reveal('.home-img',{delay:350, origin:'right'})
 
-sr.reveal('.main-box,.about,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.main-box,.about,#contact',{delay:200, origin:'bottom'})
